2013a728bd814dde3fc679c8694542c0564ea758
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-03-22 16:01:44 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //Make from bracketsConfig array of brackets
+  let bracketsArray = bracketsConfig.map(item => item.join(''));
+  //Main part
+  for (let i = 0; i < bracketsArray.length; i++) {
+    if (str.includes(bracketsArray[i])) {
+      str = str.replace(bracketsArray[i], '');
+      i = -1;
+    }
+  }
+  //Return true/false
+  if (str) {
+    return false;
+  } else {
+    return true;
+  }
 }
